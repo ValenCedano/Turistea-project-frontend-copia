040341c0ee31b18bb45282306853c4761476c369
Valentina Cedano <valentina.cedano57@gmail.com>
2024-05-23 09:56:50 -0500

    cambios 3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit to 1000 kB
+    chunkSizeWarningLimit: 1000, // Adjust as necessary
   },
+  base: '/Turistea-project-frontend-copia/',
 });
